d84750aab71fd22487af35441d2dc222a7e276d1
Jasper McLaughlin <jasper-mclaughlin@outlook.de>
2024-04-08 19:27:53 +0200

    Updated Rock Paper Scissors

---
+++ javascript/rockpaperscissors/tempCodeRunnerFile.js
@@ -0,0 +1,24 @@
+function getComputerChoice() {
+  var choices = ["Rock", "Paper", "Scissors"];
+  var randomChoice = Math.floor(Math.random() * choices.length);
+  return choices[randomChoice];
+}
+
+function singleRound(playerSelection, computerSelection) {
+    playerSelection = toLowerCase(playerSelection)
+  if (
+    (playerSelection == "Rock" && computerSelection == "Scissors") ||
+    (playerSelection == "Paper" && computerSelection == "Rock") ||
+    (playerSelection == "Scissors" && computerSelection == "Paper")
+  ) {
+    return "You win! " + playerSelection + " beats " + computerSelection;
+  } else if (playerSelection == computerSelection) {
+    return "It's a tie. Go again.";
+  } else {
+    return "You lose. " + computerSelection + " beats " + playerSelection;
+  }
+}
+
+const playerSelection = prompt("Is your choice Rock, Paper or Scissors? ");
+const computerSelection = getComputerChoice();
+console.log(singleRound(playerSelection, computerSelection));
